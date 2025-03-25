31ee64c75a53bf491bead752fc58876156f120ae
tgorsk15 <tgbusiness463@gmail.com>
2025-03-25 16:11:10 -0500

    Create testFunc

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <limits>
 
+int testFunc()
+{
+    std::cout << "Hello every one, welcome to my life";
+}
+
 int main()
 {
     std::cout <<"Here is some text.";
+
+    testFunc();
 
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     return 0;
 }
+
