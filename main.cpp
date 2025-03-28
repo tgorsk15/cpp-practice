91b758d743f11c8b55dfb1d51b78c2482320b348
tgorsk15 <tgbusiness463@gmail.com>
2025-03-28 14:26:37 -0500

    create side.cpp and configuration file

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <limits>
 
-int testFunc()
-{
-    std::cout << "Hello every one, welcome to my life";
-}
+// int testFunc()
+// {
+//     std::cout << "Hello every one, welcome to my life";
+// }
 
 int main()
 {
-    std::cout <<"Here is some text.";
+    std::cout << "Enter two numbers separated by a space: "; // ask user for a number
 
-    testFunc();
+    int x = 0;
+    int y = 0;
+           // define variable x to hold user input (and value-initialize it)
+    std::cin >> x >> y; // get number from keyboard and store it in variable x
 
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
+    std::cout << "You entered " << x << " and " << y << '\n';
     return 0;
 }
 
---
+++ side.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    int x;
+
+    std::cout << x << '\n';
+
+    return 0;
+}
